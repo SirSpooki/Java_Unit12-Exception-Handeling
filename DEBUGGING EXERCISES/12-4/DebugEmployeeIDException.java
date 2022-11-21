24c1b681b49c3d9f83a9a4f92c0ff5d746d297af
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-11-21 12:31:24 -0600

    Add files via upload

---
+++ DEBUGGING EXERCISES/12-4/DebugEmployeeIDException.java
@@ -0,0 +1,20 @@
+// The class name should be FixDebugEmployeeIDException.
+// This class extends the Exception class.
+
+public class DebugEmployeeIDException extends Exception
+
+{
+
+//public DebugEmployeeIDException()
+
+/* The constructor of the class takes a String parameter */
+
+public DebugEmployeeIDException(String s)
+
+{
+
+super(s);
+
+}
+
+}
