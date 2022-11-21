5be25b9a1e0642b7cbdb0bd072a23b1e4dfe300f
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-11-21 12:24:47 -0600

    Add files via upload

---
+++ Exercise 12-9/GradeException.java
@@ -0,0 +1,11 @@
+public class GradeException extends Exception {
+
+public static char validGradeLetters[] = {'A', 'B', 'C', 'D', 'F', 'I'};
+
+public GradeException(String string) {
+
+super(string);
+
+}
+
+}
