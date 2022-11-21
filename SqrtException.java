d8f0aaf4134989760e999948449a6862c8fe68eb
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-11-21 12:18:55 -0600

    Add files via upload

---
+++ SqrtException.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lisa
+ */
+public class SqrtException {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number;
+
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a positive number: ");
+            number = input.nextInt();
+            if (number < 0)
+                throw new ArithmeticException();
+            System.out.println("The square root of " + number + " is " + Math.sqrt(number));
+        } catch (ArithmeticException e) {
+            System.out.println("Can't take square root of negative number.");
+        }
+    }
+}
